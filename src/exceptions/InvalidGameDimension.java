93416b92e96a0b91e567749be7ba43ab97b5a281
Rahul Sharma <rahulsharma@Rahuls-MacBook-Air.local>
2024-06-16 02:29:33 +0530

    Structure of Tic Tac Toe

---
+++ src/exceptions/InvalidGameDimension.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class InvalidGameDimension extends Exception {
+    public InvalidGameDimension(String message) {
+        super(message);
+    }
+}
---
+++ src/exceptions/InvalidNumberOfPlayers.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class InvalidNumberOfPlayers extends Exception{
+    public InvalidNumberOfPlayers(String message) {
+        super(message);
+
+    }
+}
